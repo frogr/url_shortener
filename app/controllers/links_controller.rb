7f31d4fdcea12b0d1d1ad24d46a1511460f31ea6
frogr <austindanielfrench@gmail.com>
2023-11-07 16:47:04 -0800

    cleanup

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,26 +1,10 @@
 require 'securerandom'
 
 class LinksController < ApplicationController
-  before_action :set_link, only: %i[ show edit update destroy ]
+  before_action :set_link, only: %i[ show destroy ]
 
-  # GET /links or /links.json
   def index
     @links = Link.select { |x| x.link_id != nil }
-  end
-
-  # GET /links/1 or /links/1.json
-  def show
-    puts @link.url
-    redirect_to @link.url, allow_other_host: true
-  end
-
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
-
-  # GET /links/1/edit
-  def edit
   end
 
   def id_search
@@ -32,14 +16,22 @@ class LinksController < ApplicationController
     end
   end
 
-  # POST /links or /links.json
+  def show
+    url = @link.url
+    url = "https://#{url}" unless url.starts_with?('http://', 'https://')
+    redirect_to url, allow_other_host: true
+  end
+
+  def new
+    @link = Link.new
+  end
+
   def create
     @link = Link.new(link_params.merge(link_id: SecureRandom.uuid))
 
     respond_to do |format|
       if @link.save
-        format.html { redirect_to link_url(@link), notice: "Link was successfully created." }
-        format.json { render :show, status: :created, location: @link }
+        format.html { redirect_to root_path, notice: "Link was successfully saved." }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @link.errors, status: :unprocessable_entity }
@@ -47,20 +39,6 @@ class LinksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /links/1 or /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to link_url(@link), notice: "Link was successfully updated." }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /links/1 or /links/1.json
   def destroy
     @link.destroy!
 
@@ -71,12 +49,10 @@ class LinksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def link_params
       params.require(:link).permit(:url, :link_id)
     end
