b23cab8b1919a92d968975f3db408fd52690a3ef
frogr <austindanielfrench@gmail.com>
2023-11-09 15:50:30 -0800

    title, updated buttons

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 console.log("LOADED")
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.copy-btn').forEach(function(button) {
+    button.addEventListener('click', function(event) {
+      event.preventDefault();
+      navigator.clipboard.writeText(button.getAttribute('data-clipboard-text'))
+    });
+  });
+});
